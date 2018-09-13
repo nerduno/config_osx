3f1a19a86d657a4409afd41c90c9d6b2d509d35b
Aaron Andalman <nerduno.list@gmail.com>
2018-09-12 22:11:21 -0700

    Switched from .boto to .aws file, and remove matplotlibrc file from being linked.

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,11 +1,11 @@
-ln -s ~/Documents/config_osx/.boto ~/.boto
+ln -s ~/Documents/config_osx/.aws ~/.aws
 ln -s ~/Documents/config_osx/.bash_profile ~/.bash_profile
 ln -s ~/Documents/config_osx/.xmodmap ~/.xmodmap
 ln -s ~/Documents/config_osx/.gitconfig ~/.gitconfig
 ln -s ~/Documents/config_osx/.screenrc ~/.screenrc
 ln -s ~/Documents/config_osx/.ssh ~/.ssh
 ln -s ~/Documents/config_osx/.emacs.d ~/.emacs.d
-ln -s ~/Documents/config_osx/matplotlibrc ~/.matplotlib/matplotlibrc
+#ln -s ~/Documents/config_osx/matplotlibrc ~/.matplotlib/matplotlibrc
 
 ln -s ~/Documents/projects ~/projects
 ln -s ~/Documents/packages ~/packages
